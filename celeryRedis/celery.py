0fc401168c3d19a40a49d381e26c45998904adcc
OmarDIOP0 <odiop6170@gmail.com>
2025-03-06 18:48:14 +0000

    configuration_du_redis_celery 🚀

--- celeryRedis/celery.py
+++ celeryRedis/celery.py
@@ -2,21 +2,18 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# set the default Django settings module
-
+# Définir le module de configuration Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celeryRedis.settings')
 
-# instantiate a Celery object
+# Instancier Celery
 app = Celery('celeryRedis')
 
-# import the tasks
+# Charger la configuration depuis Django
 app.config_from_object('django.conf:settings', namespace='CELERY')
-# app.autodiscover_tasks()
+
+# Découvrir automatiquement les tâches définies dans les apps Django
 app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
-
-# celery worker
-
