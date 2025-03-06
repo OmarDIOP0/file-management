d49d9bf95ec162ad09bbee60dd1e4e61d5a501af
OmarDIOP0 <odiop6170@gmail.com>
2025-03-06 15:35:46 +0000

    configuration_du_docker 🚀

---
+++ celeryRedis/celery.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+# set the default Django settings module
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celeryRedis.settings')
+
+# instantiate a Celery object
+app = Celery('celeryRedis')
+
+# import the tasks
+app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.autodiscover_tasks()
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+# celery worker
+
