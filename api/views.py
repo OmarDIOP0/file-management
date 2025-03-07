72f3e82981035c19a82d9e0cfa225a84aa8112d7
OmarDIOP0 <odiop6170@gmail.com>
2025-03-07 18:45:40 +0000

    configuration de la view auth 🔐

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        data = {'message': 'Hello, World! bien connecte!!!'}
+        return Response(data)
+    
+
+class UserProfile(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        data = {
+            'username': user.username,
+            'email': user.email
+        }
+        return Response(data)
--- celeryRedis/settings.py
+++ celeryRedis/settings.py
@@ -175,6 +175,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# AUTH_USER_MODEL = 'authentication.User'
 #Rest Framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
