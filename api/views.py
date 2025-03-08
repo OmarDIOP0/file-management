bfc23d920e10d0237bb1c34fd38d1bb23c27d797
OmarDIOP0 <odiop6170@gmail.com>
2025-03-08 00:27:15 +0000

    ajout de la view

--- api/views.py
+++ api/views.py
@@ -2,23 +2,63 @@ from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import (MyTOPS,RegisterSerializer,UserSerializer,TeacherSerializer,StudentSerializer,RegisterSerializer)
+from .models import User, Student, Teacher
 
 # Create your views here.
-class ProtectedView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        data = {'message': 'Hello, World! bien connecte!!!'}
-        return Response(data)
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTOPS
     
 
-class UserProfile(APIView):
-    permission_classes = [IsAuthenticated]
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-    def get(self, request):
+        #creer un utilisateur
+        user_data = serializer.validated_data
+
+        user = User.objects.create_user(
+            email=user_data['email'],
+            username=user_data['username'],
+            password=user_data['password']
+        )
+        # En fonction du rôle, créer un profil Student ou Teacher
+        if user_data['role'] == 'Student':
+            Student.objects.create(user=user)
+        elif user_data['role'] == 'Teacher':
+            Teacher.objects.create(user=user)
+
+        # Retourner une réponse de succès avec les données de l'utilisateur
+        return Response({
+            'status': 'User created successfully',
+            'user': {
+                'email': user.email,
+                'username': user.username,
+                'role': user_data['role']
+            }
+        }, status=201)
+    
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]  # Seuls les utilisateurs authentifiés peuvent accéder
+
+    def get(self, request, *args, **kwargs):
+        """
+        Retourne les informations du profil de l'utilisateur connecté.
+        """
         user = request.user
-        data = {
+        profile = user.profile  # On accède au profil lié à l'utilisateur
+        
+        user_data = {
             'username': user.username,
-            'email': user.email
+            'email': user.email,
+            'full_name': profile.full_name,
+            'photo': profile.photo.url if profile.photo else None,
+            'bio': profile.bio,
+            'phone_number': profile.phone_number,
+            'location': profile.location,
+            'birth_date': profile.birth_date,
         }
-        return Response(data)
+
+        return Response(user_data, status=200)
