c8c7e2784a5d8567a83c022f73485fa76989a07b
OmarDIOP0 <odiop6170@gmail.com>
2025-03-11 18:49:04 +0000

    implementation du protecteed route

--- frontend/src/utils/protectedRoute.js
+++ frontend/src/utils/protectedRoute.js
@@ -1,12 +1,21 @@
 import { useContext } from "react";
 import AuthContext from "../context/Authcontext";
 import { Navigate } from "react-router-dom";
+import swal from "sweetalert2";
 
 const ProtectedRoute = ({children}) => {
     let {user} = useContext(AuthContext)
 
     if (!user){
-        console.log("No User Authenticated")
+        swal.fire({
+            title: "Vous devez vous connectez❌",
+            icon: "error",
+            toast: true,
+            timer: 6000,
+            position: "top-right",
+            timerProgressBar: true,
+            showConfirmButton: false,
+            });
         return <Navigate to="/login" replace />
     }
 
